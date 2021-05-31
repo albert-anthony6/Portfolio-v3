1a40fb9b2dd8b9ab2d0c39eeedb96560afceb0ec
Albert Valdes <albert.anthony1999@gmail.com>
2021-05-31 07:31:21 -0500

    Contact form AJAX

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -6,21 +6,41 @@
       method="POST"
       netlify-honeypot="bot-field"
       data-netlify="true"
+      @submit.prevent="handleSubmit"
     >
       <p class="hidden">
         <label
           >Dont' fill this out if you're a human: <input name="bot-field"
         /></label>
       </p>
-      <input type="text" name="name" id="name" placeholder="Name" />
-      <input type="email" name="email" id="email" placeholder="Email" />
-      <input type="text" name="subject" id="subject" placeholder="Subject" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Name"
+        v-model="form.name"
+      />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email"
+        v-model="form.email"
+      />
+      <input
+        type="text"
+        name="subject"
+        id="subject"
+        placeholder="Subject"
+        v-model="form.subject"
+      />
       <textarea
         name="message"
         id="message"
         cols="30"
         rows="10"
         placeholder="Message"
+        v-model="form.message"
       />
       <button type="submit">Send Message</button>
     </form>
@@ -30,6 +50,39 @@
 <script>
 export default {
   name: 'ContactSection',
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+      },
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&');
+    },
+    handleSubmit() {
+      fetch('/', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-uelencoded',
+        },
+        body: this.encode({
+          'form-name': 'contact',
+          ...this.form,
+        }),
+      })
+        .then(() => console.log('Successfully sent'))
+        .catch((error) => console.log(error));
+    },
+  },
 };
 </script>
 
